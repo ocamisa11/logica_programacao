ed5a4567dfa795bd2c2aa3a0206a50e63eee5516
LUIZ FELIPE DE SOUZA PORTUGAL <luiz.fsportugal@senacsp.edu.br>
2025-01-13 19:59:48 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ex. 02 - Escreva um program que pergunte o nome do usuário, armazene esse nome numa variável name e apresente uma mensagem de boas-vindas exibindo o nome do usuário
+
+Console.WriteLine("Informe o seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a) " + name);
---
+++ ex03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ex03 - Escrever programa que solicite quatro números inteiros para o usuário e exiba a soma dos números
+
+Console.WriteLine("Digite o primeiro número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o quarto número: ");
+int numero4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma é: " + (numero1 + numero2 + numero3 + numero4));
+
+Console.WriteLine($"A soma é:  {numero1 + numero2 + numero3 + numero4} ");
